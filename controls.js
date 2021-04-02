185b9979c9d4905cc1b594442abffcb155d34a7e
strayacode <dallascuber@gmail.com>
2021-04-02 23:59:48 +1100

    add script tags to index.html and implement checkOut function

--- controls.js
+++ controls.js
@@ -1,11 +1,15 @@
-function signOut() {
+function checkOut() {
 	// create js object which we will use like json
     let current_time = new Date();
     let day = {
         "time_of_sign_out": current_time,
     }
 
-    // send the day object to local storage
-    // this needs to change so we store the days in an array
-    setItem("Day", day);
+    // then get the days json object from local storage
+    let days = getItem("days", [])
+    // append a new day to the array
+    days.push(day)
+
+    // send the days object to local storage
+    setItem("days", days);
 }
